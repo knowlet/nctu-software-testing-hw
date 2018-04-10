4eaa00c1b397c94d53d078936e211f9b9b3563b5
knowlet <knowlet3389@gmail.com>
2018-04-10 20:12:05 +0800

    Add feature code

--- src/triangle.cc
+++ src/triangle.cc
@@ -2,4 +2,14 @@
 
 TriangleType Triangle::IsATriangle(int a, int b, int c)
 {
+    if (a < MIN || b < MIN || c < MIN || a > MAX || b > MAX || c > MAX)
+        throw TriangleException();
+    if (a + b <= c || b + c <= a || a + c <= b)
+        throw TriangleException();
+    else if (a == b && a == c)
+        return TriangleType::EQUILATERAL;
+    else if ((a == b && b != c) || (a == c && a != b) || (b == c && a != b))
+        return TriangleType::ISOSCELES;
+    else
+        return TriangleType::SCALENE;
 }
